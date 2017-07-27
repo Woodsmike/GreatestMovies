1622f8155b12db2269b8239594ee29377adfe779
Woodsmike <Woodsmike@users.noreply.github.com>
2017-07-27 15:21:28 -0400

    next step

---
+++ GreatestMovies/Models/Actor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GreatestMovies.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int ActorID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DOB { get; set; }
+        public string Nationality { get; set; }
+        public string Gender { get; set; }
+
+        public virtual ICollection<Movie> Movies { get; set; }
+
+    }
+}
